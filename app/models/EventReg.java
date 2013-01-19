46cae0382564f8c4e2193245b4a0406eaa1031eb
Matt Conroy <dcshock@gmail.com>
2013-01-18 21:13:16 -0700

    - ADd cron style parsing using quartz.

--- app/models/EventReg.java
+++ app/models/EventReg.java
@@ -1,8 +1,14 @@
 package models;
 
+import java.text.ParseException;
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.quartz.CronExpression;
 
 import play.data.validation.Constraints.Required;
 import play.db.ebean.Model;
@@ -19,6 +25,20 @@ public class EventReg extends Model {
 	public String desc;
 	
 	public Boolean lackof;
+	
+	public String timing;
+	
+	public String what;
+	
+	@Transient
+	public String getNextRun() {
+		try {
+			return new CronExpression(timing).getNextValidTimeAfter(new Date()).toString();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 	
 	public static Finder<Long, EventReg> find = new Finder<Long, EventReg>(
 	    Long.class, EventReg.class);
